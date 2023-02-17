6daed03a2d132fb8cc5d57c86749e55e13544cd2
ValeriaSanabria <Valeria.Sanabria@newsan.com.ar>
2023-02-17 13:25:32 -0300

    script

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarMenos() {
+    document.getElementById('multiCollapseExample2').style.display = 'none';
+}
+function mostrarMas() {
+    document.getElementById('multiCollapseExample2').style.display = 'block';
+}
