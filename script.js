54d67c5f7aca6c0b66f4561a27ff84d0ed5ad437
ValeriaSanabria <valeriasanabria@tdf.edu.ar>
2023-02-28 11:31:39 -0300

    final

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-// function mostrarMenos() {
-//     document.getElementById('multiCollapseExample1').style.display = 'none';
-//     document.getElementById('multiCollapseExample2').style.display = 'none';
-// }
-// function mostrarMas() {
-//     document.getElementById('multiCollapseExample2').style.display = 'block';
-//     document.getElementById('multiCollapseExample1').style.display = 'block';
-// }
-
-// function mostrarMenos() {
-//     document.querySelectorAll('div.collapse').style.display = 'none';
-// }
-// function mostrarMas() {
-//     document.querySelectorAll('div.collapse').style.display = 'block';
-// }
-
-function mostrarMenos() {
-    document.getElementById('principal').getElementsByClassName('prueba').style.display = 'none';
-}
-function mostrarMas() {
-    document.getElementById('principal').getElementsByClassName('prueba').style.display = 'block';
-}
-
-// intentar esta opcion de botones 
-// https://es.stackoverflow.com/questions/522880/problema-para-que-aplique-la-misma-funci%C3%B3n-a-varios-botones-un-js-a-un-html
-// https://es.stackoverflow.com/questions/525992/c%C3%B3mo-seleccionar-varios-elementos-con-una-misma-clase-desde-javascript-y-adem%C3%A1
-https://es.stackoverflow.com/questions/274980/clase-%C3%BAnica-por-click-javascript
+function mostrarMenos(){
+    for (let i = 0; i < document.querySelectorAll('.collapse').length; i++){
+        document.querySelectorAll('.collapse')[i].style.display = 'none';
+    }}
